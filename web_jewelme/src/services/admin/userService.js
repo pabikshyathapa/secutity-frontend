abe032fbd4ab2a990f78a5dc4e2e584ba8127aca
pabikshyathapa <pabikshyathapa@gmail.com>
2026-01-29 10:09:53 +0545

    auth

--- web_jewelme/src/services/admin/userService.js
+++ web_jewelme/src/services/admin/userService.js
@@ -50,20 +50,3 @@ export const deleteUserService = async (id) => {
     throw new Error("Failed to delete user");
   }
 };
-// import axios from "axios"
-
-
-// export const getAllUserService = () =>
-//     axios.get("/admin/user")
-
-// export const createOneUserService = (data) =>
-//     axios.post("/admin/user", data)
-
-// export const getOneUserService = (id) =>
-//     axios.get("/admin/user/" + id)
-
-// export const updateOneUserService = (id, data) =>
-//     axios.put("/admin/user/" + id, data)
-
-// export const deleteOneUserService = (id) =>
-//     axios.delete("/admin/user/" + id)
