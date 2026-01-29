93eca36eb446ba2302ce8452ffd3729f24009a7d
pabikshyathapa <pabikshyathapa@gmail.com>
2026-01-29 10:03:07 +0545

    changes

--- web_jewelme/src/hooks/useVerifyMfa.js
+++ web_jewelme/src/hooks/useVerifyMfa.js
@@ -1,38 +1,3 @@
-// import { useMutation } from "@tanstack/react-query";
-// import { verifyMfaService } from "../services/authService";
-// import { useContext } from "react";
-// import { AuthContext } from "../auth/AuthProvider";
-// import { toast } from "react-toastify";
-// import { useNavigate } from "react-router-dom";
-
-// export const useVerifyMfa = () => {
-//   const { login } = useContext(AuthContext);
-//   const navigate = useNavigate();
-
-//   return useMutation({
-//     mutationFn: verifyMfaService,
-//     mutationKey: ["verify-mfa"],
-//     onSuccess: (data) => {
-//       if (!data || !data.user || !data.token) {
-//         toast.error("Invalid MFA response from server");
-//         return;
-//       }
-
-//       login(data.user, data.token);
-
-//       toast.success("Login successful");
-
-//       if (data.user.role === "admin") {
-//         navigate("/admins/categoryy");
-//       } else {
-//         navigate("/dashboard");
-//       }
-//     },
-//     onError: (err) => {
-//       toast.error(err?.response?.data?.message || "Invalid or expired OTP");
-//     },
-//   });
-// };
 import { useMutation } from "@tanstack/react-query";
 import { verifyMfaService } from "../services/authService";
 import { useContext } from "react";
