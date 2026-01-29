52df4b38cb2aa8ac660887da3bb8dc93572371cf
pabikshyathapa <pabikshyathapa@gmail.com>
2026-01-29 10:06:35 +0545

    changes

--- web_jewelme/src/services/profileService.js
+++ web_jewelme/src/services/profileService.js
@@ -1,13 +1,3 @@
-// import api from "../api/api";
-
-// export const getProfile = async () => {
-//   const res = await api.get("/profile");
-//   return res.data;
-// };
-
-
-
-
 import api from "../api/api";
 
 export const getProfile = async () => {
